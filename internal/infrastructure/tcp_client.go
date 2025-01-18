077fd03800a7f4f8d22872283fda4393f55e9827
Joey-Boivin <joboi9898@gmail.com>
2025-01-18 11:19:25 -0500

    refactor: move send file functions to a utils.go file (#37)

--- internal/infrastructure/tcp_client.go
+++ internal/infrastructure/tcp_client.go
@@ -2,13 +2,8 @@ package infrastructure
 
 import (
 	"fmt"
-	"io"
-	"io/fs"
-	"math"
 	"net"
 	"os"
-	"path/filepath"
-	"strings"
 	"time"
 
 	"github.com/Joey-Boivin/sdisk/internal/models"
@@ -71,113 +66,13 @@ func NewTCPClient(config *TCPClientConfig) *TCPClient {
 }
 
 func (client *TCPClient) Run() {
-	files := client.walkDirectory()
+	files := walkDirectory(client.syncPath)
 
 	for _, file := range files {
-		client.sendFile(&file)
+		sendFile(&file, client.syncPath, client.connection, client.userID)
 	}
 
 	for {
 		time.Sleep(1000)
-	}
-}
-
-type FileToSend struct {
-	path  string
-	entry fs.DirEntry
-}
-
-func (client *TCPClient) walkDirectory() []FileToSend {
-	var files []FileToSend
-	_ = filepath.WalkDir(client.syncPath, func(path string, d fs.DirEntry, err error) error {
-		if d != nil && !d.IsDir() {
-			files = append(files, FileToSend{path, d})
-		}
-		return nil
-	})
-
-	return files
-}
-
-func (client *TCPClient) sendFile(file *FileToSend) {
-
-	entry := file.entry
-
-	info, err := entry.Info()
-
-	path := strings.TrimPrefix(file.path, client.syncPath)
-
-	if err != nil {
-		panic(err)
-	}
-
-	f, err := os.Open(file.path)
-	if err != nil {
-		panic(err)
-	}
-
-	chunksSize := DEFAULT_QUEUE_SIZE_BYTES - HEADER_SIZE - 24 - len(info.Name())
-	chunks := float64(info.Size()) / float64(chunksSize)
-	chunksCeil := int(math.Ceil(chunks))
-
-	sent := 0
-	read := 0
-	total := int(info.Size())
-
-	for i := 0; i < chunksCeil; i++ {
-		fileContentBuffer := make([]byte, int(math.Min(float64(total), float64(chunksSize))))
-
-		if err != nil {
-			panic(err)
-		}
-
-		read, err = f.Read(fileContentBuffer)
-
-		if err != nil {
-			if err != io.EOF {
-				panic(err)
-			}
-		}
-
-		updateJob := UpdateDataJob{
-			Total:    uint64(info.Size()),
-			Offset:   uint64(read),
-			PathLen:  uint64(len(path)),
-			Path:     path,
-			FileData: fileContentBuffer,
-		}
-
-		raw, err := updateJob.Bytes()
-
-		if err != nil {
-			panic(err)
-		}
-
-		header := JobHeader{
-			DataSize: uint16(len(raw)),
-			Version:  VERSION,
-			Encoding: EncodingNone,
-			Opcode:   UpdateData,
-		}
-
-		idAsBytes := client.userID.Bytes()
-		copy(header.id[:], idAsBytes)
-
-		job := Job{
-			Header: header,
-			Data:   raw,
-		}
-
-		toSend := job.Bytes()
-		wrote, err := client.connection.Write(toSend)
-		sent += wrote
-
-		updateJob.Offset += uint64(wrote)
-
-		if err != nil {
-			panic(err)
-		}
-
-		total -= read
 	}
 }
